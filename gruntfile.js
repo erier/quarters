951d6ce16c91f2670e5bd01247d3d6715d58a6be
Eric Rier <ericrier@eriermacpro.i.turbinegames.com>
2013-09-11 12:08:59 -0400

    Updated some logic for better development

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,12 @@
 module.exports = function(grunt) {
 	
 	grunt.initConfig({
+		clean: [
+			'img',
+			'css',
+			'js'
+		],
+		
 	  sass: {                              // Task
 	    dist: {                            // Target
 	      options: {                       // Target options
@@ -17,7 +23,15 @@ module.exports = function(grunt) {
 	  mincss: {
 	    compress: {
 	      files: {
-	        "public/cat/css/site.css": ["dist/css/*.css"]
+	        "css/site.css": ["dist/css/*.css"]
+	      }
+	    }
+	  },
+	  
+	  uglify: {
+	    my_target: {
+	      files: {
+	        'dist/js/scripts.min.js': ['build/js/scripts.js']
 	      }
 	    }
 	  },
@@ -25,13 +39,25 @@ module.exports = function(grunt) {
 	  watch: {
 	    gruntfile: {
 	      files: 'gruntfile.js',
-	      tasks: ['jshint:gruntfile'],
+	      tasks: [],
 	    },
 	    src: {
-	      files: ['lib/*.js', 'build/css/**/*.scss', '!lib/dontwatch.js'],
+	      files: ['build/js/*.js', 'build/css/**/*.sass', 'bower_components/jquery/jquery.js', 'dist/assets/img'],
 	      tasks: ['default'],
 	    }
-	  }
+	  },
+	  
+		copy: {
+		  main: {
+		    files: [
+		      {expand: true, cwd: 'vendor/bootstrap/css/', src: ['bootstrap.min.css', 'bootstrap-responsive.min.css'], dest: 'css/vendor/bootstrap/'},
+		      {expand: true, cwd: 'vendor/bootstrap/js/', src: ['bootstrap.min.js'], dest: 'js/vendor/bootstrap/'},
+		      {expand: true, cwd: 'vendor/jquery/', src: ['jquery.min.js'], dest: 'js/vendor/jquery/'},
+		      {expand: true, cwd: 'dist/assets/img/', src: ['**/*'], dest: 'img'},
+		      {expand: true, cwd: 'dist/js/', src: ['**/*'], dest: 'js'}
+		    ]
+		  }
+		}
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-sass');
@@ -39,9 +65,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
   
-	grunt.registerTask('default', ['sass', 'mincss', 'watch', 'clean', 'jshint']);
+	grunt.registerTask('default', ['uglify', 'clean', 'sass', 'mincss', 'copy', 'watch']);
 };
